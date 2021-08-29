d5c59cc120749b8d6629fee0f02592e79978b150
Dhiraj57 <58966263+Dhiraj57@users.noreply.github.com>
2021-08-29 17:37:22 +0530

    Basic grid system

---
+++ Assets/Scripts/GridBuildingSystem.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GridBuildingSystem : MonoBehaviour
+{
+    private void Start()
+    {
+        Grid grid = new Grid(4, 2, 10f, Vector3.zero);
+    }
+}
---
+++ Assets/Scripts/GridSystem.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Grid 
+{
+    private int width;
+    private int height;
+    private float cellSize;
+    private Vector3 originPosition;
+    private int[,] gridArray;
+
+    public Grid(int width, int height, float cellSize, Vector3 originPosition)
+    {
+        this.width = width;
+        this.height = height;
+        this.cellSize = cellSize;
+        this.originPosition = originPosition;
+
+        gridArray = new int[width, height];
+
+        for (int x=0; x < gridArray.GetLength(0); x++)
+        {
+            for(int y=0; y < gridArray.GetLength(1); y++)
+            {
+                Debug.DrawLine(GetWorldPosition(x, y), GetWorldPosition(x, y + 1), Color.white, 100f);
+                Debug.DrawLine(GetWorldPosition(x, y), GetWorldPosition(x + 1, y), Color.white, 100f);
+            }
+        }
+
+        Debug.DrawLine(GetWorldPosition(0, height), GetWorldPosition(width, height), Color.white, 100f);
+        Debug.DrawLine(GetWorldPosition(width, 0), GetWorldPosition(width, height), Color.white, 100f);
+    }
+
+    private Vector3 GetWorldPosition(int x, int y)
+    {
+        return new Vector3(x, y) * cellSize + originPosition;
+    }
+
+    private Vector2Int GetXY(Vector3 worldPosition, int x, int y)
+    {
+        x = Mathf.FloorToInt((worldPosition - originPosition).x  / cellSize);
+        y = Mathf.FloorToInt((worldPosition - originPosition).y / cellSize);
+        return new Vector2Int(x, y);
+    }
+
+}
